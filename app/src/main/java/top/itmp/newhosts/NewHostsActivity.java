34b22df8c5655ae469ac7f2114a293b297740e10
leehz <hz@huaixiaoz.com>
2016-01-05 13:06:50 +0800

    menu updated

--- app/src/main/java/top/itmp/newhosts/NewHostsActivity.java
+++ app/src/main/java/top/itmp/newhosts/NewHostsActivity.java
@@ -102,13 +102,14 @@ public class NewHostsActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                mViewPager.setCurrentItem(1);
+                break;
+            case R.id.action_about:
+                mViewPager.setCurrentItem(2);
+                break;
         }
-
         return super.onOptionsItemSelected(item);
     }
 

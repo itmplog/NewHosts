1ab1bade33a31429e7ce2c42dc640aeeac830e4f
leehz <hz@huaixiaoz.com>
2016-02-23 11:09:38 +0800

    reset instead of delete & NewHostsFragment update

--- app/src/main/java/top/itmp/newhosts/NewHostsActivity.java
+++ app/src/main/java/top/itmp/newhosts/NewHostsActivity.java
@@ -3,16 +3,12 @@ package top.itmp.newhosts;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.preference.PreferenceFragment;
-import android.support.annotation.Nullable;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 
 public class NewHostsActivity extends AppCompatActivity {
--- app/src/main/java/top/itmp/newhosts/NewHostsFragment.java
+++ app/src/main/java/top/itmp/newhosts/NewHostsFragment.java
@@ -1,10 +1,14 @@
 package top.itmp.newhosts;
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 /**
  * Created by hz on 2016/2/23.
@@ -13,7 +17,34 @@ public class NewHostsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_new_hosts, container, false);
+        Button reset = (Button)rootView.findViewById(R.id.reset);
+        Button update = (Button)rootView.findViewById(R.id.update);
+        TextView version = (TextView)rootView.findViewById(R.id.versionInfo);
+        TextView output = (TextView)rootView.findViewById(R.id.output);
+        version.setTextColor(Color.parseColor("#FF3F51B5"));
+        output.setTextColor(Color.parseColor("#FFFF4081"));
+        output.setText(getString(R.string.helpMessage));
+
+        checkVersionInfo(version);
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         return rootView;
+    }
+
+    public void checkVersionInfo(TextView textView){
+
     }
 }
